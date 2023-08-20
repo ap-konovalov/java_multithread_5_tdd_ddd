1b587e8a54254ba5e282cd6f5fcd48487e47f3fc
APKonovalov <APKonovalov@alfabank.ru>
2023-08-20 23:25:09 +0300

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -5,4 +5,8 @@ package ru.netology;
  <a href="https://github.com/netology-code/jd-homeworks/tree/video/tdd_ddd">GitHub link</a>
  **/
 public class PhoneBook {
+
+    public int add(String name, String phone){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    private PhoneBook PHONE_BOOK = null;
+
+    @BeforeEach
+    void init() {
+        PHONE_BOOK = new PhoneBook();
+    }
+
+    @Test
+    void addShouldReturnCountOfContacts() {
+        int actualCountOfContacts = PHONE_BOOK.add("Alex", "89035542345");
+        assertEquals(1, actualCountOfContacts);
+    }
+
+    @Test
+    void addShouldNotAddDuplicateNames() {
+        String duplicatedName = "Alex";
+        PHONE_BOOK.add(duplicatedName, "89035542345");
+        int actualCountOfContacts = PHONE_BOOK.add(duplicatedName, "5553535");
+        assertEquals(1, actualCountOfContacts);
+    }
 }
