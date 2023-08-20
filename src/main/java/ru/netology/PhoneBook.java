7d443cecaa6cdae71f0a6f3a79b03aefb46ca3f4
APKonovalov <APKonovalov@alfabank.ru>
2023-08-20 23:32:07 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,12 +1,21 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- Exercise:
- <a href="https://github.com/netology-code/jd-homeworks/tree/video/tdd_ddd">GitHub link</a>
+ * Exercise:
+ * <a href="https://github.com/netology-code/jd-homeworks/tree/video/tdd_ddd">GitHub link</a>
  **/
 public class PhoneBook {
 
-    public int add(String name, String phone){
-        return 0;
+    private static final Map<String, String> namesAndPhones = new HashMap<>();
+
+    public int add(String name, String phone) {
+        if (namesAndPhones.containsKey(name)) {
+            return namesAndPhones.size();
+        }
+        namesAndPhones.put(name, phone);
+        return namesAndPhones.size();
     }
 }
